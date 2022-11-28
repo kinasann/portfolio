1faa558823d20efd624bc9c6260a9fda088775cb
Kina <sanakisann10702@gmail.com>
2022-11-28 11:28:39 +0900

    first commit

---
+++ portfolio.py
@@ -0,0 +1,147 @@
+import streamlit as st
+from PIL import Image
+
+st.write('<h6 style="color:white">Top</h6>', unsafe_allow_html=True)
+st.title("ポートフォリオ")
+"""
+# tkinter Tool
+"""
+with st.sidebar:
+        st.write('<a href="#python-50">■ 使用言語、制作時間、使用モジュール</a>', unsafe_allow_html=True)
+        st.write('<a href="#content">■ 内容</a>', unsafe_allow_html=True)
+        st.write('<a href="#trigger">■ 作成に至ったきっかけ</a>', unsafe_allow_html=True)
+        st.write('<a href="#purpose">■ 目的、趣旨</a>', unsafe_allow_html=True)
+        st.write('<a href="#devised">■ 特に工夫したところ</a>', unsafe_allow_html=True)
+        with st.beta_expander("各項目事の内容、工夫した点"):
+                st.write('<a href="#left-window">★左ウインドウを</a>', unsafe_allow_html=True)
+                st.write('<a href="#geomentry">★geomentry</a>', unsafe_allow_html=True)
+                st.write('<a href="#place">★place配置</a>', unsafe_allow_html=True)
+                st.write('<a href="#color">★color</a>', unsafe_allow_html=True)
+        st.write('<div style="text-align: right"><a href="#top">一番上へ</a></div>', unsafe_allow_html=True)
+
+img_top = Image.open('tkinter_tool top.png')
+st.image(img_top, caption="tkinter Tool起動時", use_column_width=True)
+
+
+st.write('<h4 id=python>■ 使用言語 &emsp; Python &emsp;&emsp; &emsp;&emsp; ■ 制作時間 &emsp; 約50時間</h4>', unsafe_allow_html=True)
+st.write('<h4>■ 使用モジュール</h4>', unsafe_allow_html=True)
+st.write("&nbsp;tkinter ,&nbsp; pyperclip (コピー用) &nbsp;, OS (画像読み込み用)", unsafe_allow_html=True)
+st.write("")
+
+st.write('<h6 style="color:white">content</h6>', unsafe_allow_html=True)
+st.write('<h4>■ 内容</h4>', unsafe_allow_html=True)
+"""
+tkinterでデスクトップアプリを作る際に活用できるツールアプリ
+画面の右端にメニュー画面を固定し、対象のボタンを押すことで調べることが出来る。
+アプリを立ち上げると、左画面にヘルプが表示。右側にメニューが表示される。
+"""
+
+st.write('<h6 style="color:white">Trigger</h6>', unsafe_allow_html=True)
+st.write('<h4>■ 作成に至ったきっかけ</h4>', unsafe_allow_html=True)
+st.write("・tkinterでデスクトップアプリを作る際に、geomentryやplaceの引数を目安で記入し、実行して確かめる → \
+        思うサイズや位置ではなく、再度記入と実行…  等、微調整を繰り返すことが多かった。")
+st.write("・色を設定する場合、redなどの単語指定ではなく、好みの色を設定する場合、毎回インターネットで検索する必要があった。")
+st.write("上記のような微調整に時間が掛かり、制作がなかなか進まず、一目でわかるようなツールがあれば作りやすいのではないかと思い、\
+        今回の作成に至った。")
+
+st.write('<h6 style="color:white">Purpose</h6>', unsafe_allow_html=True)
+st.write('<h4>■ 目的、趣旨</h4>', unsafe_allow_html=True)
+st.write("・geomentry引数 → ウインドウを好きなサイズや位置に動かすことで、引数の入手")
+st.write("・place引数 → ルーラーを作成し、照らし合わせることで座標がわかる")
+st.write("・color →色選択ダイアログで好きな色を選択し、カラーコードを入手")
+
+st.write('<h6 style="color:white">devised</h6>', unsafe_allow_html=True)
+st.write('<h4 id="a">■ 特に工夫したところ</h4>', unsafe_allow_html=True)
+st.write("・シンプルで使いやすいアプリになるよう心掛けた。")
+st.write("・右端のメニューウインドウがメインウインドウ。左側に出る各ウインドウがサブウインドウに設定。\
+        左側にウインドウが展開したままであっても、メニューウインドウを閉じると全てのウインドウが閉じてアプリが終了する")
+st.write("・コピーボタンの作成")
+st.write("・placeルーラー(透過処理、背景変更、テキストボックスを使用したウインドウ展開等)")
+st.write("・ヘルプウインドウの作成")
+st.write("")
+st.write("")
+st.write("")
+st.write("各項目事の内容、工夫した点の詳細は下記にて説明")
+st.write("")
+
+st.write('<h6 style="color:white">left window</h6>', unsafe_allow_html=True)
+st.write('<h5>★左ウインドウを</h5>', unsafe_allow_html=True)
+st.write("<b>閉じるボタン：</b> 文字通りメニューウインドウ左に展開されるウインドウを閉じることが出来る。<br>\
+        左側にウインドウが展開されていない場合は、押すことはできるが何も反応はない。",unsafe_allow_html=True)
+st.write("")
+st.write('<h6 style="color:white">geomentry</h6>', unsafe_allow_html=True)
+st.write('<h5>★geomentry</h5>', unsafe_allow_html=True)
+img_geo =Image.open("geo_pic.png")
+st.image(img_geo, caption="geomentry", use_column_width=True)
+
+st.write("<b>ウインドウ作成：</b> 左側にウインドウを展開 <br> 通常のウインドウ操作と同様、\
+        ドラッグすることで大きさ変更、ウインドウ位置の移動をすることができる。",unsafe_allow_html=True)
+
+st.write('<b>確認ボタン：</b> 色選択ダイアログ以外の左側に展開しているウインドウの現在のサイズ、\
+        位置をテキストボックスに代入<br><span style="border-bottom:solid 3px #ff8040;">ウインドウ横幅\
+        </span> x<span style="border-bottom:solid 3px #0000ff;">ウインドウ縦幅</span> x \
+        <span style="border-bottom:solid 3px #ff80c0;">PC画面左端からウインドウまでの幅</span> x \
+        <span style="border-bottom:solid 3px #00b050;">PC画面上端からウインドウまでの幅</span><br>\
+        geomentryの引数に設定することで、同じサイズと位置でウインドウを作ることが出来る。', unsafe_allow_html=True)
+
+st.write("<b>コピーボタン：</b>文字通りコピーすることができるボタン<br>\
+        コピー対象はgeomentryラベルフレーム内のテキストボックス全範囲になっているため、\
+        一部分のみやplace位置ラベルフレーム内のテキストボックスをコピーすることは不可能<br>\
+        もし一部分のみコピーしたい場合は、コピーボタンは使用せず、通常通りテキストボックス内のコピーしたい範囲をドラックして\
+        「ctrl + c」することでコピーすることは可能",unsafe_allow_html=True)
+st.write("")
+
+st.write('<h6 style="color:white">place</h6>', unsafe_allow_html=True)
+st.write('<h5>★place配置</h5>', unsafe_allow_html=True)
+st.write("ウィジットを配置するときに使用するメソッドの一つであるplaceの座標をわかりやすく表示する物差し(ルーラー)のようなもの。")
+
+img_place =Image.open("place.png")
+st.image(img_place, use_column_width=True)
+st.write('<span style="color:#808080;">&emsp;&emsp;&emsp;&emsp; 縦軸(x=) \
+        &emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; \
+        横軸(y=)&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; \
+        方眼(x= ,y=)</span>', unsafe_allow_html=True)
+
+st.write("placeは1ピクセルごとに設定されているため、10ピクセルごとにラインを設定。<br>\
+        基本の線(10ピクセル単位)は青線、50ピクセル単位を緑線、100ピクセル単位を赤線に設定し、1000ピクセルの値には太線を引いている。<br>\
+        ウインドウを好きなサイズに変更できるようパソコン画面のサイズをプログラムが取得し、最大化してもルーラーの線が途切れないように設定している。",
+        use_column_width=True)
+st.write("作成中のウインドウを照らし\
+        合わせやすいように、ルーラー部分は透過処理を行っている。<br>\
+        そのためルーラー部分がすり抜けてしまい、サイズ調整がしづらい課題があった。<br>\
+        透過処理をしていないタイトルバーと目盛部分にカーソルを合わせて調整することで通常通り出来るが、\
+        どの部分でも調整ができる方がいいだろうと考え、スペースキーを押している間は背景色が変わるように設定。",
+        unsafe_allow_html=True)
+
+img_touka = Image.open("place_s.png")
+st.image(img_touka, use_column_width=True)
+
+
+st.write("また、作成しているウインドウサイズや位置が決まっている場合はテキストボックスに記入して\
+        調べたい軸のボタンを押すと、そのウインドウの左上に合わせた位置でルーラーが展開される")
+
+img_touka_geo = Image.open("place_geo.png")
+st.image(img_touka_geo, use_column_width=True)
+
+st.write("このプログラムを組むにあたり、テキストボックスに代入された文字を各要素に分解、計算、結合を行っている。")
+st.write("100x500などウインドウサイズのみの入力だった場合、ウインドウサイズは反映するがウインドウ位置は初期値に表示される。<br>\
+        このplaceルーラーウインドウのサイズや位置もgeomentryラベルフレームの確認ボタンで取得できるため、\
+        調べたいウインドウと重ねて確認ボタンを押すことで、位置の確認ができる。<br>\
+        その後、再度placeラベルフレームのテキスト欄に記入して対象の軸ボタンを押すと、\
+        ウインドウの左上に合わせた位置でルーラーが展開される", unsafe_allow_html=True)
+
+img_place_geo = Image.open("place_plus_geo.png")
+st.image(img_place_geo, use_column_width=True)
+st.write("")
+
+st.write('<h6 style="color:white">color</h6>', unsafe_allow_html=True)
+st.write('<h5>★color</h5>', unsafe_allow_html=True)
+st.write("<b>色ボタン：</b>ボタンを押すと、色選択ダイアログが表示 <br>\
+        本アプリ以外時の色選択ダイアログと同様、基本色にない色も設定することが出来る。<br>\
+        「OK」を押すとカラーコードと書かれた下のテキストボックスにコードが代入。<br>\
+        また、色ボタン右に選択した色を表示するように設定している。<br>", unsafe_allow_html=True)
+st.write("<b>コピーボタン：</b>geomentryと同様、カラーコードと書かれた下のテキストボックス内全てがコピー対象になる     ",unsafe_allow_html=True)
+
+
+img_color = Image.open("color2.png")
+st.image(img_color, use_column_width=True)
---
+++ tkinter_Tool.py
@@ -0,0 +1,306 @@
+import tkinter
+import pyperclip , os
+from tkinter import colorchooser
+
+scr_file = os.path.abspath(__file__) 
+os.chdir(os.path.dirname(scr_file))
+
+def btn_click(btn_ev):
+    global menu_btn , size
+    menu_btn = btn_ev.widget.cget("text")
+    if win_close == 1:
+        win.destroy()
+    if menu_btn == "ウインドウ作成":
+        size ="1000x600+145+53"
+        entry_win_size.delete(0,tkinter.END)
+        display()
+        window_size()
+    elif menu_btn == "色":
+        colorcheck()
+    elif menu_btn == "ヘルプ" :
+        left_help()
+    elif menu_btn != "閉じる" :
+        a = entry_place.get()
+        if a != "":
+            b = a.replace("x"," ")
+            c = b.replace("+"," ")
+            win_list = c.split(" ")
+            if len(win_list) == 2:
+                win_list.append("145")
+                win_list.append("53")
+            elif len(win_list) ==3:
+                win_list[2]= "145"
+                win_list.append("53")
+            elif len(win_list) == 4:
+                pass
+            else:
+                win_list = ["1000","600","100","80"]
+                
+            if menu_btn == "縦軸(x)":
+                tate = int(win_list[3])-num_y_line_h
+                win_list[3] = str(tate)
+            elif menu_btn == "横軸(y)":
+                yoko = int(win_list[2])-num_x_line_h
+                win_list[2] = str(yoko)
+            else:
+                tate = int(win_list[3])-num_y_line_h
+                win_list[3] = str(tate)
+                yoko = int(win_list[2])-num_x_line_h
+                win_list[2] = str(yoko)
+            size = ",".join(win_list)
+            size = size.replace(",","x",1)
+            size = size.replace(",","+")
+        else: size ="1000x600+145+53"
+        display()
+        left_place()
+
+def display():
+    """左ウインドウの作成"""
+    global win
+    win = tkinter.Toplevel()
+    win.focus_set() 
+    global win_close
+    win_close = win.winfo_exists()
+    win.geometry(size)
+    global frame
+    win.maxsize(width=MW, height=MH)
+    frame = tkinter.Frame(win,width=MW,height=MH, bg="#ffffaa")
+    frame.pack(fill="both") 
+    
+
+def window_size():
+    """ウインドウ作成"""
+    win.title("tkinter tool  --- geometry   ウインドウ作成   横幅 x 縦幅 + 左横から位置 + 上から位置 ")
+    global size , gazou
+    size = win.geometry()
+    gazou = tkinter.PhotoImage(file="geometry.png")
+    gazou = gazou.subsample(1)
+    label_geo = tkinter.Label(frame,image=gazou, bg="#ffffaa",height=MH)
+    label_geo.pack(fill="both",ipady=30)
+    
+   
+def  get_geometry():
+    """ウインドウ作成取得"""
+    if win_close == 1:
+        size = win.geometry()
+        if len(entry_win_size.get()) > 1:
+            entry_win_size.delete(0,tkinter.END)
+        entry_win_size.insert(0, size)
+
+
+def colorcheck():
+    if win_close == 1:
+        win.destroy()
+    color = colorchooser.askcolor()
+    
+    if color != (None, None):
+        if len(entry_cord.get()) > 1:
+            entry_cord.delete(0,tkinter.END)
+
+        global color_rgb , color_cord
+        color_cord = color[1]
+        entry_cord.insert(0,color_cord)
+        btn_color.config(state=tkinter.NORMAL)
+        label_color = tkinter.Label(frame_color, font=("MSゴシック","25"), 
+                                    anchor=tkinter.N, text="■", fg=color_cord)
+        label_color.grid(row = 0, column = 3)
+
+
+def size_copy():
+    """ウインドウの大きさをコピー"""
+    pyperclip.copy(entry_win_size.get())
+        
+def cord_copy():
+    """ウインドウの大きさをコピー"""
+    pyperclip.copy(entry_cord.get())    
+
+def key_down(e):
+    global key, line
+    key = e.keysym
+    if key == "space":
+        line.config(bg="#f9f9f9")
+
+
+def key_up(e):
+    global key, line
+    key == ""
+    line.config(bg="snow")                  
+
+
+def left_place():
+    """place配置"""
+
+    win.wm_attributes("-transparentcolor", "snow")      # 透過処理
+
+    if menu_btn == "縦軸(x)" or menu_btn == "横軸(y)" or menu_btn ==  "方眼(x,y)":
+        win.title("tkinter tool  --- placeルーラー   \
+            単位：10  緑線：50単位  赤線：100単位 (太線：1000)  大きさ変更時はスペースキーを押しながらドラッグ推奨")
+    global key
+    key = ""
+    win.bind("<KeyPress>", key_down)
+    win.bind("<KeyRelease>", key_up)    
+    
+    #* 数字の作成
+    for i in range(1000):
+        pl = i *10 
+        if i % 10 == 0 : 
+            if pl != 0:
+                num = tkinter.Label(frame,text=pl, bg="#ffffaa")
+                if menu_btn == "縦軸(x)" :
+                    num.place(x=pl-10)
+
+                elif menu_btn == "横軸(y)" :
+                    num.place(y=pl-10 )
+                elif menu_btn ==  "方眼(x,y)" :
+                    num.place(y=pl+10)
+                    num2 = tkinter.Label(frame,text=pl, bg="#ffffaa")
+                    num2.place(x=pl+10)    
+    global line                
+    line = tkinter.Canvas(win, width=MW , height=MH, bg="snow")
+ 
+    
+    if menu_btn == "縦軸(x)":
+        line.place(y=num_y_line_h )
+    elif menu_btn == "横軸(y)":
+        line.place(x=num_x_line_h)
+    else:
+        line.place(x=num_x_line_h, y=num_y_line_h)    
+        
+    for i in range(1000):
+        pl = i *10 
+        w = 1  
+        if i% 10 == 0: 
+            c = "#ff0000"
+            if i% 100 == 0:
+                w = 3
+        elif i% 5 == 0:
+            c = "green"
+        else: 
+            c = "#0000ff"
+        if menu_btn == "縦軸(x)":
+            line.create_line( pl, 0, pl, MH , fill=c, width=w)
+        elif menu_btn == "横軸(y)":
+            line.create_line( 0, pl, MW ,pl, fill=c, width=w) 
+        else:
+            line.create_line( pl, 0, pl, MH , fill=c, width=w)
+            line.create_line( 0, pl, MW ,pl, fill=c, width=w) 
+
+win_close = 0   # 左ウインドウがあるか
+win_size = 0    # サイズ確認ウインドウがあるか
+num_x_line_h = 22     # place 配置  数字 の 横の列 の高さ
+num_y_line_h = 21     # place 配置  数字 の 縦の列 の高さ
+
+#* help
+def left_help():
+    global size
+    size ="1000x640+145+30"
+    display()
+    win.title("tkinter tool  --- ヘルプ  使い方説明")
+    win.resizable(False,False)
+    
+    
+    help_dict = {" ★閉じる★ " : ["tkinter toolで開いた左側のウインドウを閉じることが可能"],
+    " ★ウインドウ作成★ " : ["左側に新しいウインドウを作成  ドラッグすることで大きさや位置を変更可能",
+        "確認ボタン → ウインドウサイズや位置をテキストボックスに表示(placeルーラーも可能)", \
+        "コピーボタン → テキストボックス内全てをコピー(範囲指定不可)"] ,
+    " ★place位置★ " : ["初期サイズ(テキストラン未記入)：「1000x600+145+53」", 
+                     "確認したいウインドウサイズをテキストボックスに記入すると、ウインドウサイズに端を合わせたplaceルーラーが表示", 
+                     "ドラッグしてウインドウ移動、サイズ変更(スペース押しながら推奨)可能",
+                     "確認がしやすいよう透過処理をしているため、動かす際は色がついている部分をクリック",
+                     "スペースキーを押すことでルーラーの背景色に変化するため、大きさ変更がしやすくなる",
+                     "１目盛10単位 緑線→50単位 赤線→100単位(太線1000", 
+                     "※ 以下の場合、初期値の「1000x600+145+53」に変更", " ・「500x100-5+10」など「-」の値がある　  \
+                    ・「fghjk」など規定以外の数値", "※ 「600x100」など位置を判定する数値がなかった場合",
+                     " →初期値の「+100+80」位置で表示"],                         
+    " ★「ウインドウ作成」機能と「place位置」機能の合わせ技★ " : 
+        ["place位置ウインドウを好きな大きさにして、geomentry欄の確認ボタンを押すと", "place位置ウインドウサイズがテキストボックスへ "],
+    " ★color★ " : ["「色」ボタンを押すと、カラーチェッカーを呼び出します。", "使いたい色を選択して、OK を押すと、テキストボックスに代入"]
+    }
+
+    for k,l in help_dict.items():
+        geo_help_frame = tkinter.LabelFrame(frame, text=k, bg="#ffffaa",font=(2) ,width=200 ,height=MH)
+        geo_help_frame.pack(pady=5, padx=10, ipady=3, ipadx=10, anchor=tkinter.W, fill="both")
+        for v in range(len(l)):
+            geo_help_text = tkinter.Label(geo_help_frame,text=l[v],bg="#ffffaa", justify="left")
+            geo_help_text.pack(anchor=tkinter.W, pady=2)     
+       
+#* メニュー
+menu = tkinter.Tk()
+menu.title("tkinter Tool menu")
+menu.resizable(0, 0)    # 最大化非表示
+menu.geometry("150x680-0+20")
+
+MW = menu.winfo_screenwidth()  # 左ウインドウ フレームの最大値(横)
+MH = menu.winfo_screenheight()    # 左ウインドウ フレームの最大値(縦)
+
+#* 閉じる
+frame_close = tkinter.LabelFrame(menu, text= "左ウインドウを")
+frame_close.pack(pady=5)
+
+close_btn = tkinter.Button(frame_close, text= "閉じる", width=10, height=2)
+close_btn.pack(pady=5, padx=5)
+close_btn.bind("<ButtonPress>", btn_click)
+
+#* win_size_btn
+frame_win_size = tkinter.LabelFrame(menu, text= "geometry")
+frame_win_size.pack(pady=5)
+
+btn_win_size = tkinter.Button(frame_win_size, text= "ウインドウ作成", width=10)
+btn_win_size.pack(pady=5, padx=5)
+btn_win_size.bind("<ButtonPress>", btn_click)
+label_win_size = tkinter.Label(frame_win_size, text="現在の左ウインドウのサイズ")
+label_win_size.pack(pady=5, padx=5)
+entry_win_size = tkinter.Entry(frame_win_size,width=20)
+entry_win_size.insert(0, "")
+entry_win_size.pack(padx=5)
+
+size_btn = tkinter.Button(frame_win_size, text="確認",command=get_geometry)
+size_btn.pack(side = tkinter.LEFT, pady=10, padx=(15,10))
+entry_win_copy = tkinter.Button(frame_win_size, text="コピー", command=size_copy)
+entry_win_copy.pack(side = tkinter.RIGHT, pady=10, padx=(10,15))
+
+
+#* place Btn
+btn_list = ["縦軸(x)", "横軸(y)", "方眼(x,y)"]
+size = "1000x600+100+80"
+
+frame_place = tkinter.LabelFrame(menu, width=10, text= "place配置")
+frame_place.pack(pady=5)
+
+label_place = tkinter.Label(frame_place, text="確認したいウインドウ")
+label_place.pack(pady=1)
+label_place2 = tkinter.Label(frame_place, text="例：1000x600+100+80\n横幅x縦幅+左から+上から")
+label_place2.pack(pady=1)
+entry_place  = tkinter.Entry(frame_place,width=18)
+entry_place.pack()
+
+for btn_num in btn_list:      #range(len(button_dict)):
+        btn = tkinter.Button(frame_place, text=btn_num)
+        btn.pack(pady=15, padx=15)
+        btn.bind("<ButtonPress>", btn_click)
+        
+#* color
+frame_color = tkinter.LabelFrame(menu, text= "color")
+frame_color.pack(pady=5)
+
+btn_color = tkinter.Button(frame_color, text= "色", width=10, state="normal",command=colorcheck)
+btn_color.grid(row = 0, column = 0, columnspan=3, pady=5, padx=5)
+
+label_cord = tkinter.Label(frame_color, text="カラーコード")
+label_cord.grid(row = 1, column = 0)
+
+entry_cord  = tkinter.Entry(frame_color,width=14)
+entry_cord.grid(row = 2, column = 0, columnspan=3, pady=5, padx=5)
+entry_cord_copy = tkinter.Button(frame_color, text="コピー", command=cord_copy)
+entry_cord_copy.grid(row = 2, column = 3)
+
+#* menu_help
+frame_help = tkinter.Frame(menu, width=20)
+frame_help.pack(pady=5)
+
+btn_help = tkinter.Button(frame_help, text="ヘルプ", width=10,bg= "#bdbdbd")
+btn_help.pack(padx=15)
+btn_help.bind("<ButtonPress>", btn_click)
+left_help()
+
+tkinter.mainloop()
